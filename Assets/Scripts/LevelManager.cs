0d39ce3f39c97e254fb4f96b28fb39984ad22cc3
Gabriel B. Gallagher <zeinfandel@yahoo.com>
2018-02-14 23:40:15 -0500

    First commit

---
+++ Assets/Scripts/LevelManager.cs
@@ -0,0 +1,35 @@
+﻿using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class LevelManager : MonoBehaviour
+{
+    public float autoLoadNextLevelAfter;
+
+    void Start()
+    {
+    	if (autoLoadNextLevelAfter==0)
+        {
+    		Debug.Log("Level auto load disabled");
+    	}
+        else
+        {
+			Invoke("LoadNextLevel", autoLoadNextLevelAfter);
+		}
+    }
+
+    public void LoadLevel(string name)
+    {
+		Debug.Log("New level load:" + name);
+		SceneManager.LoadScene(name);
+    }
+
+	public void Quit()
+    {
+		Application.Quit();
+    }
+
+    public void LoadNextLevel()
+    {
+		SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 1);
+    }
+}
