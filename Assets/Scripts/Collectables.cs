15a3afd8aa38d55253fa523ce2609398857c97a3
Gabriel B. Gallagher <zeinfandel@yahoo.com>
2018-02-19 15:07:40 -0500

    added Collectable and interactable scripts

--- Assets/Scripts/Collectables.cs
+++ Assets/Scripts/Collectables.cs
@@ -1,4 +1,10 @@
-﻿using System.Collections;
+﻿/* Author Gabriel B. Gallagher February 19th 2018
+ * 
+ * Controls the logic for the in scene collectables. Allows player to click on items in game and 
+ * performs logic, such as calling the animation and storing them in the inventory
+ */
+
+using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
