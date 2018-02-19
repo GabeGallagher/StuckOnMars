f8d3e5b7197ef714f2f2c4fb1fc4918430fad665
Gabriel B. Gallagher <zeinfandel@yahoo.com>
2018-02-19 15:28:38 -0500

    added exit door script

---
+++ Assets/Scripts/Interactables.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Interactables : MonoBehaviour
+{
+    public List<GameObject> text;
+
+    private void OnMouseDown()
+    {
+        Debug.Log("Do something with text, or something");
+    }
+}
