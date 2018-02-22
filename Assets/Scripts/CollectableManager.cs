4e5ba417660ea44ed46f42bf43b0ffb27e9c2400
Gabriel B. Gallagher <zeinfandel@yahoo.com>
2018-02-22 14:06:45 -0500

    Added collectable manager script which will allow players to select and place collectable in inventory on game objects their meant to go to advance the story

--- Assets/Scripts/CollectableManager.cs
+++ Assets/Scripts/CollectableManager.cs
@@ -1,4 +1,10 @@
-﻿using System.Collections;
+﻿/* Author Gabriel B. Gallagher February 22 2018
+ * 
+ * Manages which collectables are selected from inventory and allows the player to place collectables
+ * from inventory into objects they are meant to go in order to advnace the story
+ */
+
+using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
