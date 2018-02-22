069b5ead6a567a92f657a4a79aeb949e7c8bedac
Gabriel B. Gallagher <zeinfandel@yahoo.com>
2018-02-21 20:10:15 -0500

    Added Generator Interactables

---
+++ Assets/Scripts/Generator.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Generator : Interactables
+{
+    public override void OnMouseDown()
+    {
+        if (!base.hasPower)
+        {
+            StartCoroutine(base.ShowElement(base.text[0]));
+        }
+    }
+}
