c908faac52fbcb12ebb97918333ce728379cb8ef
Gabriel B. Gallagher <zeinfandel@yahoo.com>
2018-02-26 10:23:06 -0500

    Items now print their name at the top of the screen. Code is working as expected but throwing a null reference that I need to look into and clean up

--- Assets/Scripts/Collectables/Collectable.cs
+++ Assets/Scripts/Collectables/Collectable.cs
@@ -16,11 +16,57 @@ public class Collectable : MonoBehaviour
 
     public GameObject inventorySlot = null;
 
+    public int waitSeconds = 3;
+
     public bool isPlaced = false, isSelected = false, isCollected = false;
+
+    GameObject nameTextParent;
+
+    private void Start()
+    {
+        nameTextParent = GameObject.Find("CollectablesText");
+    }
+
+    //shows text items when the player presses interactable
+    public IEnumerator ShowElement(GameObject textItem)
+    {
+        textItem.SetActive(true);
+        yield return new WaitForSeconds(waitSeconds);
+        textItem.SetActive(false);
+    }
+
+    //Shows the name of this collectable at top of canvas when the player clicks on it
+    void ShowName()
+    {
+        GameObject nameText = null;
+
+        for (int i = 0; i < nameTextParent.transform.childCount; ++i)
+        {
+            if (nameTextParent.transform.GetChild(i).name == name)
+            {
+                Debug.Log("Collectable name: " + name);
+                Debug.Log("Child name: " + nameTextParent.transform.GetChild(i).name);
+                nameText = nameTextParent.transform.GetChild(i).gameObject;
+                Debug.Log("Text name: " + nameText.name);
+            }
+            StartCoroutine(ShowElement(nameText));
+        }
+
+        if (nameText != null)
+        {
+            nameText.SetActive(true); 
+        }
+        else
+        {
+            Debug.Log("There was an error getting the name text");
+        }
+    }
     
     public virtual void OnMouseDown()
     {
         inventory.Deselect();
+
+        ShowName();
 
         if (!isCollected)
         {
--- Assets/Scripts/Inventory.cs
+++ Assets/Scripts/Inventory.cs
@@ -12,26 +12,20 @@ public class Inventory : MonoBehaviour
 {
     public void Place(GameObject collectable)
     {
-        Debug.Log("Placing: " + collectable.name);
-
         GameObject targetSlot = new GameObject();
 
         for (int i = 0; i < transform.childCount; ++i)
         {
             GameObject slot = transform.GetChild(i).gameObject;
-            Debug.Log("At " + slot.name);
             bool hasCollectable = false;
 
             for (int j = 0; j < slot.transform.childCount; ++j)
             {
                 Transform child = slot.transform.GetChild(j);
 
-                Debug.Log("At " + slot.transform.GetChild(j));
-
                 if (child.GetComponent<Collectable>())
                 {
                     //if we find a collectable in this slot, exit the loop
-                    Debug.Log("Found a collectable");
                     hasCollectable = true;
                     j = slot.transform.childCount;
                 }
